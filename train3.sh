0c307ffaf5a1b1262d2d80e9e57dca8dc566d7e0
Frederic Odermatt <odermafr@lo-login-01.leonhard.ethz.ch>
2020-11-07 23:39:42 +0100

    Initial commit

--- train3.sh
+++ train3.sh
@@ -1,13 +1,21 @@
+source ~/NLP/.nlp_env/bin/activate
+
+cd "$(dirname "$0")"
+
+cat /proc/driver/nvidia/version
+
 TOTAL_NUM_UPDATES=200000  #1200
 WARMUP_UPDATES=500      
 LR=3e-05
 MAX_TOKENS=100     #1600
 UPDATE_FREQ=1
-BART_PATH=bart.large/model.pt
+BART_PATH=./bart.large/model.pt
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train preprocess_taskC_data/subtaskC_data-bin \
+echo "CUDA_VISIBLE_DEVICES: " $CUDA_VISIBLE_DEVICES
+
+fairseq-train ./preprocess_taskC_data/subtaskC_data-bin \
     --restore-file $BART_PATH \
-    --save-dir ./checkpoints/new \
+    --save-dir $SCRATCH/KaLM/ \
     --max-tokens $MAX_TOKENS \
     --task translation \
     --source-lang source --target-lang target \
@@ -29,6 +37,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train preprocess_taskC_data/subtaskC_data-b
     --find-unused-parameters \
     --ddp-backend=no_c10d \
     --disable-validation \
-    --log-format simple 2>&1 | tee train.log
+    --log-format simple 2>&1 | tee ~/train.log
 
 #--no-epoch-checkpoints \
